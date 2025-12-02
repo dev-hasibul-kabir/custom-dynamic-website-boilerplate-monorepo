b2eb112d24ca54d6909a6998b17ccd6dcfd8f188
Maruf Hossain <mhtamun@gmail.com>
2025-12-02 19:03:41 +0600

    Remove sensitive data logging from DbService constructor to enhance security

--- apps/api/src/db/db.service.ts
+++ apps/api/src/db/db.service.ts
@@ -7,7 +7,6 @@ import { PrismaClient } from '@prisma/client';
 export class DbService extends PrismaClient {
   constructor(readonly config: ConfigService) {
     const connectionString = config.get('POSTGRES_DATABASE_URL');
-    console.log(connectionString);
     const adapter = new PrismaPg({ connectionString });
 
     super({
